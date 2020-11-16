2ea48c7eab50fbdd6d8c643c2c20443c1433c9c8
theCryon <66066260+theCryon@users.noreply.github.com>
2020-11-16 12:26:34 +0100

    Version 5/6 finished update

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -5,59 +5,147 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
-    public static String Encrypt(String text, int key){
+interface Strategy {
+    public String Encrypt(String text, int key);
+    public String Decrypt(String text, int key);
+}
+
+class ConcreteStrategyShift implements Strategy{
+
+    @Override
+    public String Encrypt(String text, int key){
+        StringBuilder encrypted = new StringBuilder();
+        char c;
+        for (int i = 0; i < text.length(); i++){
+            if (text.charAt(i) == ' ') {
+                encrypted.append(' '); }
+            if (text.charAt(i) >= 65 && text.charAt(i) <= 90) {
+                if (text.charAt(i) + key > 90) {
+                    c = (char) (text.charAt(i) - 26);
+                    encrypted.append((char) (c + key));
+                } else {
+                    encrypted.append((char) (text.charAt(i) + key));
+                }
+            } else if (text.charAt(i) >= 97 && text.charAt(i) <= 122) {
+                if (text.charAt(i) + key > 122) {
+                    c = (char) (text.charAt(i) - 26);
+                    encrypted.append((char) (c + key));
+                } else {
+                    encrypted.append((char) (text.charAt(i) + key));
+                }
+            }
+        }
+        return encrypted.toString();
+    }
+
+    @Override
+    public String Decrypt(String text, int key){
+        StringBuilder decrypted = new StringBuilder();
+        char c;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) >= 65 && text.charAt(i) <= 90) {
+                if (text.charAt(i) - key < 65) {
+                    c = (char) (text.charAt(i) + 26);
+                    decrypted.append((char) (c - key));
+                } else {
+                    decrypted.append((char) (text.charAt(i) - key));
+                }
+            } else if (text.charAt(i) >= 97 && text.charAt(i) <= 122) {
+                if (text.charAt(i) - key < 97) {
+                    c = (char) (text.charAt(i) + 26);
+                    decrypted.append((char) (c - key));
+                } else {
+                    decrypted.append((char) (text.charAt(i) - key));
+                }
+            }
+        }
+        return decrypted.toString();
+    }
+}
+
+class ConcreteStrategyUnicode implements Strategy {
+
+    @Override
+    public String Encrypt(String text, int key){
         StringBuilder encrypted = new StringBuilder();
         for (int i = 0; i < text.length(); i++){
             encrypted.append((char) (text.charAt(i) + key));
         }
         return encrypted.toString();
     }
-    public static String Decrypt(String text, int key){
+
+    @Override
+    public String Decrypt(String text, int key){
         StringBuilder decrypted = new StringBuilder();
         for (int i = 0; i < text.length(); i++){
             decrypted.append((char) (text.charAt(i) - key));
         }
         return decrypted.toString();
     }
-    public static void Logic(String[] args) {
-        String operation = "";
-        int key = 0;
-        String data = "";
-        boolean toFile = false;
-        String filePath = "";
+}
+
+class Context {
+    private Strategy strategy;
+
+    private String mode;
+    private int key;
+    private String data;
+    private String alg;
+
+    private boolean toFile;
+    private String filePath;
+
+    public void setStrategy(String[] args) {
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "-mode":
-                    operation = args[i + 1];
+                    this.mode = args[i + 1];
                     break;
                 case "-key":
-                    key = Integer.parseInt(args[i + 1]);
+                    this.key = Integer.parseInt(args[i + 1]);
                     break;
                 case "-data":
-                    data = args[i + 1];
+                    this.data = args[i + 1];
                     break;
                 case "-in":
                     File file = new File(args[i + 1]);
                     try (Scanner sc = new Scanner(file)) {
-                        data = sc.nextLine();
+                        this.data = sc.nextLine();
                     } catch (IOException e) {
                         System.out.println("Error reading from the file at: "+args[i+1]);
                     }
                     break;
                 case "-out":
-                    toFile = true;
-                    filePath = args[i+1];
+                    this.toFile = true;
+                    this.filePath = args[i+1];
+                    break;
+                case "-alg":
+                    this.alg = args[i+1];
                     break;
             }
         }
+    }
+
+    public void executeStrategy() {
         String message = "";
-        switch (operation) {
+        switch (mode) {
             case "enc":
-                message = Encrypt(data, key);
+                if (alg.equals("unicode")) {
+                    ConcreteStrategyUnicode unicode = new ConcreteStrategyUnicode();
+                    message = unicode.Encrypt(data, key);
+                } else {
+                    ConcreteStrategyShift shift = new ConcreteStrategyShift();
+                    message = shift.Encrypt(data, key);
+                }
                 break;
             case "dec":
-                message = Decrypt(data, key);
+                if (alg.equals("unicode")) {
+                    ConcreteStrategyUnicode unicode = new ConcreteStrategyUnicode();
+                    message = unicode.Decrypt(data, key);
+                } else {
+                    ConcreteStrategyShift shift = new ConcreteStrategyShift();
+                    message = shift.Decrypt(data, key);
+                }
                 break;
             default:
                 System.out.println("Oops something went wrong :c");
@@ -71,9 +159,14 @@ public class Main {
             }
         }
         System.out.println(message);
-
     }
+}
+
+
+public class Main {
     public static void main(String[] args) {
-            Logic(args);
+            Context context = new Context();
+            context.setStrategy(args);
+            context.executeStrategy();
     }
 }
